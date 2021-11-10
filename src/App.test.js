7ef2a40a76f196e3e2294664de43404084452757
Swislok-Dev <cmarker4@gmail.com>
2021-11-10 09:20:04 -0600

    tested a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+// import ReactTestUtils from 'react-dom/test-utils'
 
-test('renders learn react link', () => {
+test('has h1 element tag', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  // expect().toBeInTheDocument();
+  const homepage = document.querySelector("h1")
+  expect(homepage.textContent).toBe("Home page")
 });
