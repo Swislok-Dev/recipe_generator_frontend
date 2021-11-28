03d02ce1b380a47d6d2619cec53497fea7bbe723
Swislok-Dev <cmarker4@gmail.com>
2021-11-27 22:55:41 -0600

    merge changes back

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-import store from 'store'
+import { BrowserRouter as Router } from 'react-router-dom'
+import store from './store'
 
 
 
@@ -18,7 +17,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
