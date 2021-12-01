3944352b0ac89259cb10a504ea2440b6c2931367
Swislok-Dev <cmarker4@gmail.com>
2021-11-30 20:38:39 -0600

    include compose and proper work redux store

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { manageRecipes } from './redux/manageRecipes'
 
-import store from 'store'
 
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(manageRecipes, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
     <Router><Provider store={store}>
       <App />
-    </Provider></Router>,
+    </Provider></Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
