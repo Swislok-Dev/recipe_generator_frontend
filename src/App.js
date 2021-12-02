f2105547dff3c893d25d7d230d8c16ee31228d5c
Swislok-Dev <cmarker4@gmail.com>
2021-12-02 09:31:01 -0600

    clean up imports and path to recipe page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
-// import CreateRecipe from './components/CreateRecipe'
 import { Route, Switch } from 'react-router-dom'
 import UserForms from './components/Users/UserForms'
 import RecipesContainer from './containers/RecipesContainer'
-import RecipeSearch from './components//Recipes/RecipeSearch'
 
-// import { connect } from 'react-redux'
-// 
 
 class App extends Component {
   render () {
@@ -22,7 +18,7 @@ class App extends Component {
         <Switch>
           <Route path="/login" component={ UserForms } />
           <Route path="/recipes/new" />
-          <Route path="/recipes" render={() => <RecipeSearch />}/>
+          <Route path="/recipes" />
           <Route path="/" component={ Home } />
         </Switch>
       </div>
