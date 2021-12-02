c1225e9c162c7ba651147bad06247e606f63669e
Swislok-Dev <cmarker4@gmail.com>
2021-12-02 09:28:51 -0600

    rename Recipe to RecipePage

---
+++ src/components/Recipes/RecipePage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+// import { connect } from 'react-redux'
+
+// const recipesList = this.props.recipes
+
+// class Recipe extends React.Component {
+  const RecipePage = (props) => {
+  const  {recipes, match}  = props
+  
+  const showProps = () => {
+    console.log("Recipes from Recipe.props:", (props))
+  }
+
+  const recipeList = (
+    recipes.map(recipe => 
+      <li key={recipe.id}>{recipe.title}</li>)
+  )
+
+
+  return (
+    <div>
+      <h1>Recipes should all be here</h1>
+      <button onClick={showProps}>Show PropS</button>
+      <ul>
+        
+        {recipeList}
+        
+
+      </ul>
+      
+
+    </div>
+  )
+}
+
+export default RecipePage
