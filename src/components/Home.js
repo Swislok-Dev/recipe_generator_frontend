680670afad019ed9654223e6ec209f8e37459069
Swislok-Dev <cmarker4@gmail.com>
2021-12-04 15:13:21 -0600

    Give some directions on landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <h1>Home Route</h1>
+      <h1>Find a recipe or create your own!</h1>
+      <p>Browse through recently added recipes or search for one</p>
+      <p>Search by listing an ingredient or the name of a given recipe</p>
+      <p>Create a recipe that you think would be a good addition</p>
     </div>
   )
 }
