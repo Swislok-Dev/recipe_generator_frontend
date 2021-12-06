27c0e3d750b222455e8939ee888c0b3192f169ea
Swislok-Dev <cmarker4@gmail.com>
2021-12-06 08:15:09 -0600

    render recipe cards now

--- src/components/Recipes/RecipeList.js
+++ src/components/Recipes/RecipeList.js
@@ -3,21 +3,23 @@ import { Link } from 'react-router-dom'
 
 const RecipeList = ({ recipes }) => {
   const renderRecipes = Object.keys(recipes).map((recipeID) => (
-    <li><Link key={recipeID} to={`/recipes/${recipeID}`}>
-      {recipes[recipeID].title}
-    </Link></li>
+    <div className="recipe-card">
+      <Link key={recipeID} to={`/recipes/${recipeID}`}>
+        {recipes[recipeID].title}
+      </Link>
+    </div>
   ))
 
-  // const recipeList = (
-  //   recipes.map(recipe => 
-  //     <li key={recipe.id}>{recipe.title}</li>)
+  // const renderRecipes = (
+  //   recipes.map(recipe =>
+  //     <Link key={recipe.id} {...recipe} to={`/recipes/${recipe.id}`} >
+  //       <li>{recipe.title}</li>
+  //     </Link>)
   // )
 
-  console.log("RecipeList props", recipes)
   return (
     <div>
       {renderRecipes}
-      {/* {recipeList} */}
     </div>
   )
 }
