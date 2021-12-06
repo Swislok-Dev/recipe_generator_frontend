3dbd9d78f61cdab76ef36f66c20bc91e5f08be6c
Swislok-Dev <cmarker4@gmail.com>
2021-12-06 08:17:01 -0600

    add route to recipe show page

--- src/containers/RecipesContainer.js
+++ src/containers/RecipesContainer.js
@@ -4,6 +4,7 @@ import { fetchRecipes  } from '../redux/recipesActions'
 import RecipePage from '../components/Recipes/RecipePage'
 import CreateRecipe from '../components/Recipes/CreateRecipe'
 import { Switch, Route } from 'react-router-dom'
+import RecipeShow from '../components/Recipes/RecipeShow'
 
 
 class RecipesContainer extends React.Component {
@@ -12,24 +13,14 @@ class RecipesContainer extends React.Component {
     this.props.fetchRecipes()
   }
 
-  handleClick = () => {
-    console.log(this.props)
-  }
-  
-    // console.log("RecipesContainer props:", this.props)
   render(){
     return (
       <div>
-        {/* <h2>Recipes Container</h2> */}
         <Switch>
           <Route path="/recipes/new" component={ CreateRecipe } />
-          <Route path="/recipes/:id" />
+          <Route path="/recipes/:id" render={(routerProps) => <RecipeShow {...routerProps} recipes={this.props.recipes} /> }/>
           <Route path="/recipes" render={(routerProps) => <RecipePage { ...routerProps } recipes={this.props.recipes} />} />
         </Switch>
-        {/* <RecipeSearch /> */}
-
-        {/* <Recipe recipes={this.props.recipes} /> */}
-        {/* {this.recipes.map((recipe) => <Recipe key={recipe.id} {...recipe} />)} */}
       </div>
     )
   }
@@ -38,11 +29,5 @@ class RecipesContainer extends React.Component {
 const mapStateToProps = (state) => {
   return ({ recipes: state.recipes })
 }
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     addIngredient: formData => dispatch({ type: 'ADD_INGREDIENT', payload: formData})
-//   }
-// }
 
 export default connect(mapStateToProps, { fetchRecipes })(RecipesContainer)
