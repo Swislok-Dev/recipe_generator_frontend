d255e75e006f8464696e604f8ff5c1f427b2356f
Swislok-Dev <cmarker4@gmail.com>
2021-12-08 20:03:35 -0600

    add styling template for forms

--- src/components/Recipes/CreateRecipe.js
+++ src/components/Recipes/CreateRecipe.js
@@ -9,17 +9,6 @@ class CreateRecipe extends React.Component {
     ingredients: "",
     instructions: "",
   }
-
-  // addIngredient = () => {
-  //   let { ingredients, input } = this.state;
-  //   ingredients.push(input);
-  // }
-
-  // saveIngredient = (e) => {
-
-  //   this.setState({ [e.target.name]: e.target.value })
-  // }
-
   
   onHandleChange = e => {
     this.setState({
@@ -29,10 +18,7 @@ class CreateRecipe extends React.Component {
   
   onHandleSubmit = (e) => {
     e.preventDefault();
-    // const { title, ingredients, instructions } = this.props
     this.props.addRecipe(this.state)
-    console.log("this.state", this.state)
-    console.log("this.props.state:", this.props.recipes)
     this.setState({
       title: "",
       ingredients: "",
@@ -48,50 +34,48 @@ class CreateRecipe extends React.Component {
     console.log("Button clicked to show state:", this.state)
   }
 
-  addIngredientButton = document.getElementById("addIngredientBtn")
-
-  // ingredientsList = () => {
-  //   this.props.ingredients.map((items, idx) => {
-  //     return <li key={idx}>{items}</li>
-  //   })
-  // }
-
   render() {
     return (
-      <div >
-        <h1>Create Recipe Page</h1>
+      <div className="background">
         <div>
-          <button onClick={this.showState}>Show State</button>
-          <button onClick={this.showProps}>Show Props</button><br />
+          {/* <button onClick={this.showState}>Show State</button>
+          <button onClick={this.showProps}>Show Props</button><br /> */}
         </div><br />
 
-        <form className="recipe-form" onSubmit={this.onHandleSubmit}>
-          
+        <div className="container">
+          <div className="wrap-container">
+            <form className="form-title validate-form" onSubmit={this.onHandleSubmit}>
+              <span className="form-title">Create Recipe Page</span>
 
-          <label htmlFor="title">Recipe Name</label>
-          <input type="text" required name="title" placeholder="Name your recipe" value={this.state.title} onChange={this.onHandleChange} />
+              <div className="wrap-input validate-input alert-validate" data-validate="Title is required">
+                <label htmlFor="title">Recipe Name</label>
+                <input className="input has-val" type="text" required name="title"  value={this.state.title} onChange={this.onHandleChange} />
+                <span className="focus-input" data-placeholder="RECIPE TITLE"></span>
+              </div>
 
-          <label htmlFor="ingredients">Ingredients</label>
-          <input
-          type="text"
-          required
-          name="ingredients"
-          placeholder="Ingredients"
-          value={this.state.ingredients}
-          onChange={this.onHandleChange}
-          />
-          
-          <label htmlFor="instructions">Instructions</label>
-          <textarea
-          type="text"
-          required
-          name="instructions"
-          placeholder="Instructions"
-          value={this.state.instructions}
-          onChange={this.onHandleChange}
-          />
-          <input type="submit" />
-          </form>
+              <div className="wrap-input validate-input alert-validate" data-validate="Ingredients are required">
+                <label htmlFor="ingredients">Ingredients</label>
+                <input className="input" type="text" required name="ingredients" value={this.state.ingredients} onChange={this.onHandleChange} />
+                <span className="focus-input has-val" data-placeholder="INGREDIENTS"></span>
+              </div>
+
+              <div className="wrap-input validate-input alert-validate" data-validate="Instructions are required">
+                <label htmlFor="instructions">Instructions</label>
+                <textarea className="input" type="text" required name="instructions" value={this.state.instructions} onChange={this.onHandleChange} />
+                <span className="focus-input has-val" data-placeholder="INSTRUCTIONS"></span>
+              </div>
+              <div className="container-form-btn">
+                <div className="wrap-form-btn" type="submit">
+                  <div className="form-bgbtn">
+                    <input className="form-btn" type="submit" value="Submit your recipe" />
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+
+        </div>
+
       </div>
     )
   }
