3dd7e56b128451dbeb6e0427e9b0e115e01207a5
Swislok-Dev <cmarker4@gmail.com>
2021-12-08 20:05:04 -0600

    get list to show correct route and page

--- src/components/Recipes/RecipeList.js
+++ src/components/Recipes/RecipeList.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const RecipeList = ({ recipes }) => {
-  const renderRecipes = Object.keys(recipes).map((recipeID) => (
-    <div className="recipe-card">
-      <Link key={recipeID} to={`/recipes/${recipeID}`}>
-        {recipes[recipeID].title}
-      </Link>
-    </div>
-  ))
+const RecipeList = props => {
+  console.log("RecipeList() rendered", props)
+  const { recipes } = props
 
-  // const renderRecipes = (
-  //   recipes.map(recipe =>
-  //     <Link key={recipe.id} {...recipe} to={`/recipes/${recipe.id}`} >
-  //       <li>{recipe.title}</li>
-  //     </Link>)
-  // )
+  const renderRecipes = (
+    recipes.map(recipe =>
+      <Link key={recipe.id}  to={`/recipes/${recipe.id}`} >
+        <li>{recipe.title}</li>
+      </Link>)
+  )
 
   return (
     <div>
