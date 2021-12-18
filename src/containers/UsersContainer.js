4751ab001952f5ff4aa23e75da13555fa87d2289
Swislok-Dev <cmarker4@gmail.com>
2021-12-17 19:56:37 -0600

    setup container for users to fetch, log in and out

---
+++ src/containers/UsersContainer.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import SignupForm from '../components/Users/signupForm'
+import UserForm from '../components/Users/UserForm'
+import { loginStatus } from '../redux/usersActions'
+
+class UsersContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoggedIn: false,
+      user: {}
+    }
+  }
+  
+  componentDidMount() {
+    // loginStatus() 
+    console.log("Users Container tried to mount the loginStatus()", )
+  }
+  
+
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user``
+    })
+  }
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {}
+    })
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/login" component={ UserForm } />
+            {/* <Route path='/signup' component={SignupForm} /> */}
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
+
+export default UsersContainer
+
