994b8a79fe7dba0360f8ad289bf58874d49246f3
Swislok-Dev <cmarker4@gmail.com>
2021-12-19 01:04:12 -0600

    make navlinks one line

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,25 +17,10 @@ const activeLink = {
 const NavBar = (props) =>{
   return (
     <nav className="navbar">
-      <NavLink
-      to="/login"
-      exact
-      style={link}
-      activeStyle={activeLink}
-      >
-        Login</NavLink>
-      <NavLink
-      to='/'
-      exact
-      style={link}
-      activeStyle={
-        activeLink
-      }
-      >
-        Home
-      </NavLink>
+      <NavLink to='/' exact style={link} activeStyle={activeLink} >Home</NavLink>
       <NavLink to="/recipes" exact style={link} activeStyle={activeLink}>Recipes</NavLink>
       <NavLink to='/recipes/new' exact style={link} activeStyle={activeLink}>Create Recipe</NavLink>
+      <NavLink to="/login" exact style={link} activeStyle={activeLink}>Login</NavLink>
     </nav>
   )
 }
