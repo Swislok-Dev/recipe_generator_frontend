fba6326a512d2b0af48ad27724034a3a60ac6481
Swislok-Dev <cmarker4@gmail.com>
2021-12-29 20:36:05 -0600

    fix spacing

--- src/redux/recipesReducer.js
+++ src/redux/recipesReducer.js
@@ -3,7 +3,7 @@ export function recipesReducer(state = {recipes: []}, action) {
   switch (action.type) {
     
     case 'GET_RECIPES':
-      return {  recipes: action.payload} 
+      return { recipes: action.payload } 
 
     case 'ADD_RECIPE':
       return { ...state, recipes: [...state.recipes, action.payload] }
