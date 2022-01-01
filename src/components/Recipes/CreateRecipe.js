f802b05bda5e62d73ee8c1db55e6ea3500e9aaa3
Swislok-Dev <cmarker4@gmail.com>
2021-12-31 21:33:12 -0600

    clean up

--- src/components/Recipes/CreateRecipe.js
+++ src/components/Recipes/CreateRecipe.js
@@ -35,31 +35,26 @@ class CreateRecipe extends React.Component {
       <div className="background">
         <div className="container">
           <div className="wrap-container">
-            <form className="form-title validate-form" onSubmit={this.onHandleSubmit}>
+            <form className="form validate-form" onSubmit={this.onHandleSubmit}>
               <span className="form-title">Create Recipe Page</span>
 
               <div className="wrap-input validate-input " data-validate="Title is required">
-                {/* <label htmlFor="title">Recipe Name</label> */}
                 <input className="input"  type="text" required name="title"  value={this.state.title} onChange={this.onHandleChange} />
                 <span className="focus-input" data-placeholder="RECIPE TITLE"></span>
               </div>
 
               <div className="wrap-input validate-input " data-validate="Ingredients are required">
-                {/* <label htmlFor="ingredients">Ingredients</label> */}
                 <input className="input" type="text" required name="ingredients" value={this.state.ingredients} onChange={this.onHandleChange} />
                 <span className="focus-input " data-placeholder="INGREDIENTS"></span>
               </div>
 
               <div className="wrap-input validate-input " data-validate="Instructions are required">
-                {/* <label htmlFor="instructions">Instructions</label> */}
                 <textarea className="input" type="text" required name="instructions" value={this.state.instructions} onChange={this.onHandleChange} />
                 <span className="focus-input " data-placeholder="INSTRUCTIONS"></span>
               </div>
               <div className="container-form-btn">
                 <div className="wrap-form-btn" type="submit">
-                  {/* <div className="form-bgbtn"> */}
                     <input className="form-btn" type="submit" value="Submit your recipe" />
-                  {/* </div> */}
                 </div>
               </div>
             </form>
