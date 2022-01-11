150a365af566e9059ac3c19e9ef2bd234b3d04bb
Swislok-Dev <cmarker4@gmail.com>
2022-01-11 15:57:10 -0600

    convert to functional component with hooks

--- src/components/Reviews/ReviewForm.js
+++ src/components/Reviews/ReviewForm.js
@@ -1,77 +1,82 @@
 // import { useState } from 'react'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { connect } from 'react-redux'
 import { addReview, fetchRecipes } from '../../redux/actions'
 
-// const ReviewForm = ({ recipeId, props}) => {
-  class ReviewForm extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        rating: 5,
-        content: '',
-      }
-    }
+const ReviewForm = ({ recipe, fetchRecipes, addReview, recipes, props}) => {
+  // class ReviewForm extends React.Component {
+    //   constructor(props) {
+      //     super(props)
+      //     this.state = {
+        //       rating: 5,
+        //       content: '',
+        //     }
+        //   }
 
-    componentDidMount() {
-      this.props.fetchRecipes()
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-      if (this.props === prevProps && this.state !== prevState) {
-        this.props.fetchRecipes()
-      }
-    }
-
-    componentWillUnmount() {
-      this.props.fetchRecipes()
-    }
-
-  onHandleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  onHandleSubmit = e => {
+  const [rating, setRating] = useState(5)
+  const [content, setContent] = useState('')
+  
+  const onHandleSubmit = e => {
     e.preventDefault()
-    this.props.addReview(this.state, this.props.recipe.id)
-    this.setState({
-      rating: 5,
-      content: '',
-    })
+    const newReview = { rating, content }
+    addReview(newReview, recipe.id)
+    setRating(5)
+    setContent('')
   }
-  // const [rating, setRating] = useState(5)
-  // const [content, setContent] = useState('')
 
-  // const onHandleSubmit = e => {
-  //   e.preventDefault()
-  //   const newReview = { rating, content }
-  //   addReview(newReview, recipeId)
-  //   setRating(5)
-  //   setContent('')
+  useEffect(fetchRecipes, [fetchRecipes, rating, content, ])
+
+
+  //   componentDidMount() {
+  //     this.props.fetchRecipes()
+  //   }
+
+  //   componentDidUpdate(prevProps, prevState) {
+  //     if (this.props === prevProps && this.state !== prevState) {
+  //       this.props.fetchRecipes()
+  //     }
+  //   }
+
+  //   componentWillUnmount() {
+  //     this.props.fetchRecipes()
+  //   }
+
+  // onHandleChange = e => {
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   })
   // }
 
-  render() {
+  // onHandleSubmit = e => {
+  //   e.preventDefault()
+  //   this.props.addReview(this.state, this.props.recipe.id)
+  //   this.setState({
+  //     rating: 5,
+  //     content: '',
+  //   })
+  // }
+
+
+  // render() {
     return (
-    <form className='review-container' onSubmit={this.onHandleSubmit}>
+    <form className='review-container' onSubmit={onHandleSubmit}>
       <h3>Add a review for this recipe</h3>
       <label>
         Rating:
-        <input type='number input' name='rating' onChange={this.onHandleChange} value={this.state.rating} />
+        <input type='number input' name='rating' onChange={(e) => setRating(e.target.value)} value={rating} />
       </label>
 
       <label>
         Content:
-        <input type='text input' name='content' onChange={this.onHandleChange} value={this.state.content} />
+        <input type='text input' name='content' onChange={(e) => setContent(e.target.value)} value={content} />
       </label>
       <input type='submit' value='Submit Review' />
     </form>
   )
 
 }
-  }
+  // }
 
 const mapStateToProps = state => ({ recipes: state.recipes })
 
