89a0a33db8cfc402b3b035d80a96974408c0f242
Swislok-Dev <cmarker4@gmail.com>
2022-01-13 14:05:07 -0600

    switch to use hooks

--- src/components/Recipes/CreateRecipe.js
+++ src/components/Recipes/CreateRecipe.js
@@ -1,56 +1,55 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useState,  } from 'react'
+import { useHistory } from 'react-router-dom'
+import { connect, useDispatch } from 'react-redux'
 import { addRecipe } from '../../redux/actions'
 
-class CreateRecipe extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: "",
-      ingredients: "",
-      instructions: "",
-    }
-  }
 
-  onHandleChange = e => {    
-    this.setState({
-      [e.target.name]: e.target.value,
-    })
+const CreateRecipe = () => {
 
+  const dispatch = useDispatch();
+  const history = useHistory()
+
+  const [inputs, setInputs] = useState({})
+
+  const handleChange = (e) => {
+    setInputs(values => ({...values, [e.target.name]: e.target.value,}))
     e.target.value !== "" ? e.target.classList.add("has-val") : e.target.classList.remove("has-val")
+
   }
-  
-  onHandleSubmit = (e) => {
-    e.preventDefault();
-    this.props.addRecipe(this.state)
-    this.setState({
-      title: "",
-      ingredients: "",
-      instructions: "",
-    })
-    this.props.history.push("/recipes")
-  }  
-  
-  render() {
+
+  const { title, ingredients, instructions } = inputs
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(addRecipe(inputs))
+    history.push('/recipes/')
+  }
+
+
     return (
       <div className="background">
         <div className="container">
           <div className="wrap-container">
-            <form className="form validate-form" onSubmit={this.onHandleSubmit}>
+            <form className="form validate-form" onSubmit={handleSubmit}>
               <span className="form-title">Create Recipe Page</span>
 
               <div className="wrap-input validate-input " data-validate="Title is required">
-                <input className="input"  type="text" required name="title"  value={this.state.title} onChange={this.onHandleChange} />
+                <input className="input"  type="text" required name="title"  value={title} onChange={handleChange} />
+                {/* <input className='input' type='text' required name='title' value={title} onChange={onChange} /> */}
                 <span className="focus-input" data-placeholder="RECIPE TITLE"></span>
               </div>
 
               <div className="wrap-input validate-input " data-validate="Ingredients are required">
-                <input className="input" type="text" required name="ingredients" value={this.state.ingredients} onChange={this.onHandleChange} />
+                <input className="input" type="text" required name="ingredients" value={ingredients} onChange={handleChange} />
+                {/* <input className='input' type='text' required name='ingredients' value={ingredients} onChange={onChange} /> */}
+
                 <span className="focus-input " data-placeholder="INGREDIENTS"></span>
               </div>
 
               <div className="wrap-input validate-input " data-validate="Instructions are required">
-                <textarea className="input" type="text" required name="instructions" value={this.state.instructions} onChange={this.onHandleChange} />
+                <textarea className="input" type="text" required name="instructions" value={instructions} onChange={handleChange} />
+                {/* <textarea className='input' type='text' required name='instructions' value={instructions} onChange={onChange} /> */}
+
                 <span className="focus-input " data-placeholder="INSTRUCTIONS"></span>
               </div>
               <div className="container-form-btn">
@@ -64,13 +63,12 @@ class CreateRecipe extends React.Component {
       </div>
     )
   }
-}
 
-const mapStateToProps = state => {
-  return {
-    recipes: state.recipes
-  }
-}
+
+const mapStateToProps = state => ({ 
+  recipes: state.recipes ,
+
+})
 
 
 export default connect(mapStateToProps, { addRecipe })(CreateRecipe)
