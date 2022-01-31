49c5685913298307211b4a1227da39e2825a9112
Swislok-Dev <cmarker4@gmail.com>
2022-01-31 11:35:20 -0600

    write search function for recipes

--- src/components/Recipes/RecipeList.js
+++ src/components/Recipes/RecipeList.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import { useState } from 'react'
+
 import { Link } from 'react-router-dom'
 
 const RecipeList = props => {
 
   const { recipes } = props
+  const [filterRecipes, setFilterRecipes] = useState([]) 
 
-  const renderRecipes = (
-    recipes.map(recipe =>
+
+  const searchRecipes = (event) => {
+    const search = event.target.value.toLowerCase()
+    console.log("Search:", search)
+    const filterRecipes = recipes.filter(recipe => {
+      const foundRecipes = recipe.title.toLowerCase().includes(search)
+      return foundRecipes
+    })
+    setFilterRecipes(filterRecipes)
+
+  }
+  
+
+  const renderRecipes = () => {
+    const showRecipes = (filterRecipes.length === 0) ? recipes : filterRecipes
+    return  showRecipes.map(recipe =>
       <Link key={recipe.id} className="card"  to={`/recipes/${recipe.id}`}>
         <p>{recipe.title}</p>
       </Link> 
     )
-  )
+  }
 
   return (
     <div className="display-recipes">
       <h2>Choose a recipe below</h2>
+      <input type="text" placeholder="search" onChange={searchRecipes}/>
       <div className="cards">
-        {renderRecipes}
+        {renderRecipes()}
         </div>
     </div>
   )
