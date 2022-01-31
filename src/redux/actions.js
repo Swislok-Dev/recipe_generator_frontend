4446b1c06ef96d5e1f0e4aa79ccd5f810ea49a46
Swislok-Dev <cmarker4@gmail.com>
2022-01-31 11:36:25 -0600

    include braces for dispatch

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -5,8 +5,9 @@ export const fetchRecipes = () => {
   return dispatch => {
     fetch(`${API}/recipes`)
     .then(resp => resp.json())
-    .then(recipes => dispatch({ type: 'GET_RECIPES', payload: recipes }))
-
+    .then(recipes => {
+      dispatch({ type: 'GET_RECIPES', payload: recipes })
+    })
   }
 }
 
@@ -46,46 +47,7 @@ export const addReview = (review, recipeId) => {
   })
 }
 
-export const signup = (user) => {
-  return dispatch => fetch(`${API}/users`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    }, body: JSON.stringify(user),
-  })
-  .then(resp => handleResponse(resp, dispatch))
-}
 
-export const login = (user) => {
-  return dispatch => fetch(`${API}/sessions`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(user),
-  })
-  .then(resp => handleResponse(resp, dispatch))
-}
-
-export const logout = () => {
-  return dispatch => {
-    localStorage.clear()
-    dispatch({ type: 'LOGOUT'})
-  }
-}
-
-function handleResponse(response, dispatch) {
-  if (response.ok) {
-    response.json()
-    .then(resp => {
-      localStorage.token = resp.token
-      dispatch({ type: 'GET_USER', payload: resp.user})
-    })
-  } else {
-    response.json()
-    .then(resp => alert(resp.errors))
-  }
-}
 
 
 
