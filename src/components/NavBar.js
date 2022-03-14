d187dbe12c1ed0a7b54c907a67300e4fcd927987
Swislok-Dev <cmarker4@gmail.com>
2022-03-14 15:23:31 -0500

    append bootstrap scripts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,7 +17,7 @@ const activeLink = {
 
 const NavBar = () =>{
   return (
-    <nav className="navbar">
+    <nav id="navbar">
       <NavLink to='/' exact style={link} activeStyle={activeLink} >Home</NavLink>
       <NavLink to="/recipes" exact style={link} activeStyle={activeLink}>Recipes</NavLink>
       <NavLink to='/recipes/new' exact style={link} activeStyle={activeLink}>Create Recipe</NavLink>
